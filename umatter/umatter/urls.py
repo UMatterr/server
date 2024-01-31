3fb5be9ec8d401d8a9bd586a4dc54463aef6fe25
HoonCantona <dreampath88@gmail.com>
2024-02-01 01:06:48 +0900

    fix url

--- umatter/umatter/urls.py
+++ umatter/umatter/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user.urls')),
-    path('friends/', include('friend.urls')),
+    path('friends', include('friend.urls')),
 ]
