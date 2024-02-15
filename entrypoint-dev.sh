8bff9b7947148a7ba3f5409255d46ec7d3d95e37
HoonCantona <dreampath88@gmail.com>
2024-02-15 14:36:52 +0900

    Merge branch 'main' into dev

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ "$POSTGRES_DB" = "umatter" ]; then
+if [ "$POSTGRES_DB" = "umatter" ]
+then
     echo "Waiting for postgres..."
 
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
@@ -13,6 +14,6 @@ fi
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
 python manage.py loaddata event_initial_data.json
-gunicorn -b 0.0.0.0:8000 -w 4 --threads 4 --timeout 60 --log-level 'info' --forwarded-allow-ips '*' --proxy-allow-from '*' umatter.wsgi:application
+
+exec "$@"
