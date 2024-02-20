1ad5c0fd7c875792054d61b467a351aa7087d853
HoonCantona <dreampath88@gmail.com>
2024-02-20 12:07:20 +0900

    update exposing port

--- restart_local_db.sh
+++ restart_local_db.sh
@@ -7,15 +7,17 @@ docker ps -a
 
 docker run -d \
     --rm \
+    -e POSTGRES_USER=root \
     --env-file config/postgres/.env-dev \
-    -p 5432:5432 \
+    -p 5433:5432 \
     --name db \
     postgres:15
 
 docker run -d \
     --rm \
+    -e POSTGRES_USER=root \
     --env-file config/postgres/.env-nlp-dev \
-    -p 5433:5432 \
+    -p 5434:5432 \
     --name nlp_db \
     postgres:15
 
